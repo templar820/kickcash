d33005f8372964dd69a08ccc4f6e2f66334c953c
modagavr <49167406+modagavr@users.noreply.github.com>
2020-03-23 17:13:09 +0300

    Revert "proposal single"
    This reverts commit 3297f613c5c10f3d8f89388ce4e39661ae08d025.

--- app/src/main/java/com/example/kickcash/ui/home/HomeViewModel.kt
+++ app/src/main/java/com/example/kickcash/ui/home/HomeViewModel.kt
@@ -9,9 +9,5 @@ class HomeViewModel : ViewModel() {
     private val _text = MutableLiveData<String>().apply {
         value = "This is home Fragment"
     }
-
-
-
-
     val text: LiveData<String> = _text
 }
